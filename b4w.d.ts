03e4dbc5400b0840de4a0656781f91bb5cb44157
gastrodia <1771919544@qq.com>
2015-06-10 07:57:52 +0800

    1

--- b4w.d.ts
+++ b4w.d.ts
@@ -24,5 +24,43 @@ declare module B4W{
       autoresize:boolean;//Automatically resize canvas to match the size of container element.
     });
 
+    attr_animate(elem:Element,attr_name:string,from:number,to:number,timeout:number,opt_callback:Function);
+    css_animate(elem:Element,prop:string,from:number,to:number,timeout:number,opt_prefix:string,opt_suffix:string,opt_callback:Function);
+
+    disable_camera_controls():void;//Disable controls for the active camera.
+    disable_controls():void;//Disable engine controls.
+    disable_object_controls(obj:any):void//Disable controls for the non-camera object. obj: Object ID or Camera object ID
+    //Assign keyboard and mouse controls to the active camera. (arrow keys, ADSW, wheel and others)
+    //disable_default_pivot:Do not use the possible camera-defined pivot point应该是禁止旋转的意思
+    //disable_letter_controls:Disable keyboard letter controls (only arrow keys will be used to control the camera) 应该是禁止WASD控制，只允许上下左右键控制
+    enable_camera_controls(disable_default_pivotopt?:boolean, disable_letter_controlsopt?:boolean);
+    //Enable engine controls. Execute before using any of the controls.*() functions
+    enable_controls():void;
+    /*
+    Enable debug controls:
+    K - make camera debug shot
+    L - make light debug shot
+    M - flashback messages
+    */
+    enable_debug_controls():void
+    enable_object_controls(obj:any);
+
+    //Set the movement style for the active camera.
+    set_camera_move_style(move_style:number);
+
+    check_fullscreen():boolean;
+    exit_fullscreen():void;
+    request_fullscreen(elem:Element, enabled_cb:Function, disabled_cb:Function);
+
+
+    get_url_params(allow_param_array:boolean):Object;
+    /*
+    Report an application error. Creates standard HTML elements with error info and inserts them in the page body.
+    text_message	String	Message to place on upper element.
+    link_message	String	Message to place on bottom element.
+    link	String	Link to place on bottom element.
+    purge_elements	Array	Array of elements to destroy just before the error elements are inserted.
+    */
+    report_app_error(text_message:string, link_message:string, link:string, purge_elements:Array<any>):void;
   }
 }
